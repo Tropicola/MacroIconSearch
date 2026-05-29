431731d10be304924d91bb3f531e7ed5cd1a8c1b
Tropicola <217772737+Tropicola@users.noreply.github.com>
2026-05-29 14:04:59 +0100

    Refactor icon search logic and event hooks
    Updated GUI and Icon,Spell & Item ID search.
    
    Removed Item name search as CRAZY slow. 2fps.

--- Core.lua
+++ Core.lua
@@ -2,168 +2,155 @@ local addonName, MIS = ...
 MacroIconSearch = MIS
 
 local OIconDataProvider = nil
-local searchQuery = ""
 local icons = {}
 
 EventUtil.ContinueOnAddOnLoaded(addonName, function()
-    if not IsAddOnLoaded("MacroSetup") then
-        MIS:BuildIconCache()
-    else
-        MIS.GetIconCache = MacroSetup.GetIconCache
+    local hooks = { "MacroPopupFrame", "GearManagerPopupFrame" }
+    
+    for _, frameName in ipairs(hooks) do
+        local frame = _G[frameName]
+        if frame then
+            frame:HookScript("OnShow", function(self)
+                OIconDataProvider = self.iconDataProvider
+                MacroIconSearchFrame:ClearAllPoints()
+                MacroIconSearchFrame:SetParent(self)
+                MacroIconSearchFrame:SetPoint("TOPLEFT", self, "BOTTOMLEFT", 0, -2)
+                MacroIconSearchFrame:Show()
+            end)
+            
+            frame:HookScript("OnHide", function(self)
+                MacroIconSearchFrame:Hide()
+                MacroIconSearchFrame.SpellSearchBox:SetText("")
+                MacroIconSearchFrame.SpellIDBox:SetText("")
+                MacroIconSearchFrame.IconIDBox:SetText("")
+                MacroIconSearchFrame.ItemIDBox:SetText("")
+                wipe(icons)
+                if MIS.searchCoroutine then MIS.searchCoroutine = nil end
+            end)
+        end
     end
-
-    MacroPopupFrame:HookScript("OnShow", function(self)
-        searchQuery = ""
-        OIconDataProvider = self.iconDataProvider
-        MacroIconSearchFrame:ClearAllPoints()
-        MacroIconSearchFrame:SetParent(self)
-        MacroIconSearchFrame:SetPoint("BOTTOMLEFT", self, "TOPLEFT", 2, -5)
-        MacroIconSearchFrame:Show()
-        MacroIconSearchFrame.IconSearchQueryEditBox:SetFocus()
-    end)
-    
-    MacroPopupFrame:HookScript("OnHide", function(self)
-        MacroIconSearchFrame:Hide()
-        MacroIconSearchFrame.IconSearchQueryEditBox:SetText("")
-        wipe(icons)
-    end)
-
-    GearManagerPopupFrame:HookScript("OnShow", function(self)
-        searchQuery = ""
-        OIconDataProvider = self.iconDataProvider
-        MacroIconSearchFrame:ClearAllPoints()
-        MacroIconSearchFrame:SetParent(self)
-        MacroIconSearchFrame:SetPoint("BOTTOMLEFT", self, "TOPLEFT", 2, -5)
-        MacroIconSearchFrame:Show()
-        MacroIconSearchFrame.IconSearchQueryEditBox:SetFocus()
-    end)
-    
-    GearManagerPopupFrame:HookScript("OnHide", function(self)
-        MacroIconSearchFrame:Hide()
-        MacroIconSearchFrame.IconSearchQueryEditBox:SetText("")
-        wipe(icons)
-    end)
 end)
 
--- Will change it to a coroutine later
-local function UpdateIcons()
-    wipe(icons)
-    tinsert(icons, [[INTERFACE\ICONS\INV_MISC_QUESTIONMARK]])
-
-    local exactMatch = searchQuery:find("\"$", 1)
-    for name, icon in pairs(MIS.GetIconCache()) do
-        if (exactMatch and name == searchQuery:sub(1, exactMatch - 1)) or name:lower():find(searchQuery:lower(), 1, true) then
-            tinsert(icons, icon)
-        end
-    end
-end
-
 local IconDataProvider = CreateAndInitFromMixin(IconDataProviderMixin)
-function IconDataProvider:GetIconByIndex(index)
-    return icons[index]
-end
+function IconDataProvider:GetIconByIndex(index) return icons[index] end
+function IconDataProvider:GetNumIcons() return #icons end
 
-function IconDataProvider:GetNumIcons()
-    return #icons
-end
-
-function MIS:SetCustomIcon(editBox, text)
-    local iconID = tonumber(text)
-    if iconID then
-        local parent = MacroIconSearchFrame:GetParent()
+local function SetDirectIcon(iconID)
+    if not iconID then return end
+    local parent = MacroIconSearchFrame:GetParent()
+    if parent and parent.IconSelector then
         parent.IconSelector.selectedCallback(nil, iconID)
         parent.IconSelector:SetSelectedIndex(1)
-        parent.BorderBox.IconSelectorEditBox:SetFocus()
-        editBox:SetText("")
+        
+        MacroIconSearchFrame.SpellSearchBox:ClearFocus()
+        MacroIconSearchFrame.SpellIDBox:ClearFocus()
+        MacroIconSearchFrame.IconIDBox:ClearFocus()
+        MacroIconSearchFrame.ItemIDBox:ClearFocus()
     end
 end
 
-function MIS:OnTextChanged(editBox, isThrottle)
-    if isThrottle then
-        local parent = MacroIconSearchFrame:GetParent()
-        searchQuery = editBox:GetText()
-
-        local update = false
-        if not self.lastSearchQuery or self.lastSearchQuery ~= searchQuery then
-            update = true
-        end
-
-        if searchQuery == "" then
-            parent.iconDataProvider = OIconDataProvider
-            parent:Update()
-        else
-            if parent.iconDataProvider == OIconDataProvider then
-                parent.iconDataProvider = IconDataProvider
-                local getSelection = GenerateClosure(IconDataProvider.GetIconByIndex, parent)
-                local getNumSelections = GenerateClosure(IconDataProvider.GetNumIcons, parent)
-                parent.IconSelector:SetSelectionsDataProvider(getSelection, getNumSelections)
-            end
-
-            if update then
-                UpdateIcons()
-                parent.IconSelector:UpdateSelections()
-                self.lastSearchQuery = searchQuery
-            end
-        end
-    else
-        self.throttle = self.throttle or C_Timer.NewTimer(0.2, function()
-            MIS.throttle = nil
-            MIS:OnTextChanged(editBox, true)
-        end)
-    end
-end
-
-function MIS:OnEnterPressed()
+function MIS:ResetSearch()
     local parent = MacroIconSearchFrame:GetParent()
-    parent.BorderBox.IconSelectorEditBox:SetFocus()
+    if parent then
+        parent.iconDataProvider = OIconDataProvider
+        parent:Update()
+    end
+    if self.searchCoroutine then self.searchCoroutine = nil end
 end
 
-do 
-    local iconCache = {}
-    function MIS:BuildIconCache()
-        -- https://github.com/WeakAuras/WeakAuras2/blob/5dbf5b2c3271a256f132d25dcc7f1d6040bbb490/WeakAuras/WeakAuras.lua#L4059
-        wipe(iconCache)
-        local co = coroutine.create(function()
-            local id = 0
-            local misses = 0
-            while misses < 80000 do
-                id = id + 1
-                local name, _, icon = GetSpellInfo(id)
+-- ==========================
+-- Spell Handlers
+-- ==========================
+function MIS:OnSpellSearchEnter(editBox)
+    local text = editBox:GetText()
+    if text == "" then return self:ResetSearch() end
+    self:StartSearch("SPELL", text:lower())
+    editBox:ClearFocus()
+end
 
-                if icon == 136243 then
-                    misses = 0;
-                elseif name and name ~= "" and icon then
-                    iconCache[name] = icon
-                    misses = 0
-                else
-                    misses = misses + 1
-                end
-                coroutine.yield()
-            end
-        end)
+function MIS:OnSpellIDEnter(editBox)
+    local id = tonumber(editBox:GetText())
+    if id then
+        local sInfo = C_Spell and C_Spell.GetSpellInfo and C_Spell.GetSpellInfo(id)
+        if sInfo and sInfo.iconID then SetDirectIcon(sInfo.iconID) end
+    end
+    editBox:SetText("")
+    editBox:ClearFocus()
+end
 
+-- ==========================
+-- Icon / Item Handlers
+-- ==========================
+function MIS:OnIconIDEnter(editBox)
+    local id = tonumber(editBox:GetText())
+    if id then SetDirectIcon(id) end
+    editBox:SetText("")
+    editBox:ClearFocus()
+end
 
-        local coFrame = CreateFrame("Frame")
-        local keepRunning = true;
-        coFrame:SetScript("OnUpdate", function(self, elapsed)
-            -- Start timing
-            local start = debugprofilestop();
-            -- Resume as often as possible (Limit to 16ms per frame -> 60 FPS)
-            while (debugprofilestop() - start < 10 and keepRunning) do
-                if coroutine.status(co) ~= "dead" then
-                    local ok, msg = coroutine.resume(co)
-                    if not ok then
-                        geterrorhandler()(msg .. '\n' .. debugstack(co))
-                    end
-                else
-                    keepRunning = false
-                    coFrame:SetScript("OnUpdate", nil)
-                end
-            end
-        end);
+function MIS:OnItemIDEnter(editBox)
+    local id = tonumber(editBox:GetText())
+    if id then
+        local iIcon = C_Item and C_Item.GetItemIconByID and C_Item.GetItemIconByID(id)
+        if iIcon then SetDirectIcon(iIcon) end
+    end
+    editBox:SetText("")
+    editBox:ClearFocus()
+end
+
+-- ==========================
+-- Core Search Logic
+-- ==========================
+function MIS:StartSearch(mode, query)
+    local parent = MacroIconSearchFrame:GetParent()
+    if not parent then return end
+
+    if parent.iconDataProvider ~= IconDataProvider then
+        parent.iconDataProvider = IconDataProvider
+        parent.IconSelector:SetSelectionsDataProvider(
+            GenerateClosure(IconDataProvider.GetIconByIndex, parent), 
+            GenerateClosure(IconDataProvider.GetNumIcons, parent)
+        )
     end
 
-    function MIS.GetIconCache()
-        return iconCache
+    if self.searchCoroutine and coroutine.status(self.searchCoroutine) ~= "dead" then
+        self.searchCoroutine = nil
     end
+
+    print("|cFF00FFFFMacroIconSearch:|r Searching... Please wait.")
+
+    self.searchCoroutine = coroutine.create(function()
+        wipe(icons)
+
+        if mode == "SPELL" then
+            for id = 1, 200000 do
+                local sInfo = C_Spell and C_Spell.GetSpellInfo and C_Spell.GetSpellInfo(id)
+                if sInfo and sInfo.name and sInfo.iconID and sInfo.name:lower():find(query, 1, true) then
+                    tinsert(icons, sInfo.iconID)
+                end
+                if id % 2500 == 0 then coroutine.yield() end
+            end
+        end
+
+        parent.IconSelector:UpdateSelections()
+        print("|cFF00FFFFMacroIconSearch:|r Search complete! Found " .. #icons .. " icons.")
+    end)
+
+    local coFrame = CreateFrame("Frame")
+    coFrame:SetScript("OnUpdate", function(s)
+        if self.searchCoroutine and coroutine.status(self.searchCoroutine) ~= "dead" then
+            local startTime = debugprofilestop()
+            while debugprofilestop() - startTime < 8 do
+                local ok, err = coroutine.resume(self.searchCoroutine)
+                if not ok then 
+                    geterrorhandler()(err)
+                    s:SetScript("OnUpdate", nil)
+                    break
+                end
+                if coroutine.status(self.searchCoroutine) == "dead" then break end
+            end
+        else
+            s:SetScript("OnUpdate", nil)
+        end
+    end)
 end
